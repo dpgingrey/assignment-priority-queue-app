f488e1c4d0978286fabd4c9adf566480db9926e4
dpgingrey <dpg2208@hsutx.edu>
2024-12-04 20:01:17 -0600

    Finished Homework.java

--- Homework.java
+++ Homework.java
@@ -1,20 +1,42 @@
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Stores name, date, and notes for an Assignment.
+ *
+ * @author Dana Gingrey
+ * @version 12/04/24
+ */
+
 public class Homework {
     String assignmentName;
     LocalDate dueDate;
     String assignmentNotes;
 
+    /**
+     * Constructor for Homework
+     *
+     * @param name of Homework
+     * @param date of Homework
+     * @param notes for Homework
+     */
     public Homework(String name, String date, String notes){
         assignmentName = name;
         assignmentNotes = notes.isEmpty() ? "No notes" : notes;
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
         dueDate = LocalDate.parse(date, formatter);
-
     }
 
+    /**
+     * Getter method for the due date of Homework.
+     *
+     * @return due date of Homework
+     */
+    public LocalDate getDueDate(){
+        return dueDate;
+    }
+    
     @Override
     public String toString(){
         return "Assignment: " + assignmentName +
